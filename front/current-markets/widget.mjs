f8539773e8e4ddc31b589603232c1e830c0374ee
howellk3 <khowellmobile@gmail.com>
2023-11-22 13:33:17 -0500

    Adjust light theme colors

--- front/current-markets/widget.mjs
+++ front/current-markets/widget.mjs
@@ -1,8 +1,7 @@
 import * as theme from "./../header/theme.mjs";
-const prefixId = id => `cc_${id}`;
+const prefixId = (id) => `cc_${id}`;
 
 export const addWidget = (item) => {
-
   const container = document.createElement("div");
   container.classList.add("hidden");
   container.id = prefixId(item.id);
@@ -13,15 +12,17 @@ export const addWidget = (item) => {
     symbol: `${item.exchange}:${item.symbol}`,
     interval: item.interval,
     timezone: "Etc/UTC",
-    theme: theme.getTheme(),
+    theme: "dark",
     style: "1",
     locale: "en",
     enable_publishing: false,
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    gridColor: "rgba(0, 0, 0, 0.06)",
     save_image: false,
     container_id: container.id,
   });
-}
-export const remove = id => {
+};
+export const remove = (id) => {
   const el = document.getElementById(prefixId(id));
-  el.remove()
+  el.remove();
 };
