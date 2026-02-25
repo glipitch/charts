67b339501a677f08eff35ae6bce3d87119c4894f
glipitch <124493357+glipitch@users.noreply.github.com>
2026-02-25 07:32:27 +0000

    Update fetch-tv-data workflow, script, and assets

--- back/fetch-tv-data.js
+++ back/fetch-tv-data.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const targetDirectory = 'data';
+const targetDirectory = 'front/available-markets';
 var path = require('path');
 module.exports = async ({ fetch }) => {
     let start = 0;
@@ -15,37 +15,15 @@ module.exports = async ({ fetch }) => {
     } while (remaining > 0);
     ensureDirectory(targetDirectory);
     allData.sort((a, b) => (a.symbol > b.symbol) ? 1 : -1);
-    writeFile("full-data.json", JSON.stringify(allData));
     const reducedComboArray = reduce(allData);
     const filtered = dedupe(reducedComboArray, ({ symbol, exchange }) => `${symbol}-${exchange}`);
 
     filtered.sort((a, b) => (a.exchange > b.exchange) ? 1 : -1);
 
     writeFile("data.json", JSON.stringify(filtered));
-    //    writeExchanges(filtered);
-    //writeCrypto(allData);
-    //const result = inventory.group(({ type }) => type);
     console.log('finished');
 }
 const sleep = ms => new Promise(r => setTimeout(r, ms));
-function writeCrypto(allData) {
-    let crypto = allData.filter(item => item.hasOwnProperty('typespecs'));
-    let crypto1 = crypto.filter(item => item.typespecs.includes('crypto'));
-    const cryRed = reduce(crypto1);
-    const cryde = dedupe(cryRed, ({ symbol, exchange }) => `${symbol}-${exchange}`);
-    writeFile("crypto.json", JSON.stringify(cryde));
-}
-
-function writeExchanges(filtered) {
-    let exchanges = [];
-    filtered.forEach(item => {
-        if (!exchanges.includes(item.exchange)) {
-            exchanges.push(item.exchange);
-        }
-    });
-    exchanges.sort();
-    writeFile("exchanges.json", JSON.stringify(exchanges));
-}
 function dedupe(reducedComboArray, f) {
     const ids = reducedComboArray.map(f);
     const filtered = reducedComboArray.filter((item, index) => !ids.includes(f(item), index + 1));
@@ -67,18 +45,16 @@ function reduce(symbols) {
     });
 }
 async function fetchTvData(start = 0, fetch) {
-    let startSegment;
-    if (start == 0) {
-        startSegment = '';
-    }
-    else {
-        startSegment = `&start=${start}`;
-    }
+    let startSegment = start > 0 ? `&start=${start}` : '';
     let url = `https://symbol-search.tradingview.com/s/?text=&hl=1&exchange=&lang=en&type=${startSegment}&domain=production&sort_by_country=US`;
     return await fetchUrl(url, fetch);
 }
+const headers = {
+    'Origin': 'https://www.tradingview.com',
+    'Referer': 'https://www.tradingview.com/'
+};
 async function fetchUrl(url, fetch) {
-    const response = await fetch(url);
+    const response = await fetch(url, { headers });
     return await response.json();
 }
 function ensureDirectory(dir) {
